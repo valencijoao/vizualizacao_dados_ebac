d0db5f20a2179cea3eedb12256a7fe0a63b82545
João Valenci <valencijoao30@gmail.com>
2026-04-07 21:27:07 -0300

    Exemplo - mapa de calor com Seaborn

--- exemplo_graficos.py
+++ exemplo_graficos.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+df = pd.read_csv('clientes-v3-preparado.csv')
+
+df_corr = df[['salario','idade','anos_experiencia','numero_filhos','nivel_educacao_cod','area_atuacao_cod','estado_cod']].corr()
+# Heatmap de correlação
+plt.figure(figsize=(10,8))
+sns.heatmap(df_corr, annot=True, fmt=".2f")
+plt.title('Mapa de Calor da Correlação entre Variáveis')
+plt.show()

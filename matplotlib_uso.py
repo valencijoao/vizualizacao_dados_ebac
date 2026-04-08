7bc4b526b771f0e18bc1b1607bd3fcf92b478622
João Valenci <valencijoao30@gmail.com>
2026-04-07 21:15:05 -0300

    Estudo sobre a ferramenta seaborn

--- matplotlib_uso.py
+++ matplotlib_uso.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+df = pd.read_csv('clientes-v3-preparado.csv')
+
+print(df.head(20).to_string())
--- seaborn_uso.py
+++ seaborn_uso.py
@@ -15,7 +15,21 @@ plt.title('Densidade de Salários')
 plt.xlabel('Salário')
 plt.show()
 
-# Gráfico Pairplot - Densidade e Histograma
+# Pairplot - Densidade e Histograma
 sns.pairplot(df[['idade','salario','anos_experiencia','nivel_educacao']])
+plt.show()
+
+# Regressão 
+sns.regplot(x='idade', y='salario', data=df, color='#278f65', scatter_kws={'alpha':0.5, 'color':'#34c289'})
+plt.title('Regressão de Salário por Idade')
+plt.xlabel('Idade')
+plt.ylabel('Salário')
+plt.show()
+
+# Countplot com hue
+sns.countplot(x='estado_civil', hue='nivel_educacao', data=df, palette='pastel')
+plt.xlabel('Estado Civil')
+plt.ylabel('Quantidade de Clientes')
+plt.legend(title='Nível de Educação')
 plt.show()
 
